63af567d5a594da0d53e3ae30c7016322591b6fd
douglassilvaido <texcronos@gmail.com>
2024-06-27 16:07:56 -0300

    first commit

---
+++ Js/owl/setup.js
@@ -0,0 +1,20 @@
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin: 10,
+    responsiveClass:true,
+    responsive:{
+        0:{
+            items:3,
+            nav:false
+        },
+        600:{
+            items:5,
+            nav:false
+        },
+        1000:{
+            items:6,
+            nav:false,
+            loop:true
+        }
+    }
+})
